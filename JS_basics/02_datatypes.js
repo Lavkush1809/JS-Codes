de9d17c665e0937ad8e2f79cbea7a0a6d77bc109
Lavkush <122461964+Lavkush1809@users.noreply.github.com>
2023-12-07 07:29:29 +0000

    Datatypes

---
+++ JS_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //this will allow to use all js code as the newer version of JS
+
+//alert(3+3) -> we are working nodejs and not on browser therefore here alert will not work
+
+/*Datatypes are:-
+1- Numbers
+2- Bigint
+3-String - ""
+4-Boolean - true/false
+5- Null (standalone value) -> considered as an object in js
+6- undefiend
+7- symbols
+ */
+
+//to see the typeof of any value
+console.log(typeof 18);
