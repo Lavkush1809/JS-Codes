29d5279f5bb50ad43b789e538c1c0faf35442999
Lavkush <122461964+Lavkush1809@users.noreply.github.com>
2023-12-11 17:44:19 +0000

    datatype summary

--- JS_basics/03_typeconversion.js
+++ JS_basics/03_typeconversion.js
@@ -10,3 +10,30 @@ let changed1 = Number(score1)
 
 console.log(typeof changed1);
 console.log(changed1)
+
+//we can change one datatype to another in js and can check if chnaged or not by using typeof operation
+
+
+//**************** operations**********************
+console.log(5+6);
+console.log(5-4);
+console.log(5*6);
+console.log(15/6);
+console.log(15%6);
+console.log(2**6);
+
+let str1 = "Hello";
+let str2 = " Lavkush";
+let str3 = str1+str2;
+console.log(str3)
+
+let str4 = "1"+2;
+let str5 = 2 + "1";
+
+let str6 = "1"+2+2;
+let str7 = 2+2+"3";
+
+console.log(str4);
+console.log(str5);
+console.log(str6);
+console.log(str7);
---
+++ JS_basics/04_datatype_summary.js
@@ -0,0 +1,24 @@
+//we have  tow types of datatypes in JS
+
+//1- primitive datatype
+
+/* 
+1-Number
+2-String
+3-BigInt
+4-Boolean
+5-Symbol
+6-Undedined
+7-NULL
+*/
+
+
+//2-Non -primitive datatype
+
+/*
+1- Array
+2- Object
+3- Function
+*/
+
+//we can check the datatype of function by using "typeof"function
